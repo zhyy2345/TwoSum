14f99d488024ee760e3470af075c1ab8fee3db3e
zhyy2345 <542737263@qq.com>
2020-01-21 19:08:37 +0800

    Create TwoSum.hpp

---
+++ TwoSum.hpp
@@ -0,0 +1,20 @@
+/
+//  TwoSum.hpp
+//  TwoSum
+//
+//  Created by 张永义 on 2020/1/21.
+//  Copyright © 2020 张永义. All rights reserved.
+//
+
+#ifndef TwoSum_hpp
+#define TwoSum_hpp
+#include <iostream>
+#include <vector>
+using namespace std;
+class TwoSum
+{
+public:
+    static vector<int> twoSum1(vector<int>& nums, int target);
+    static vector<int> twoSum2(vector<int>& nums, int target);
+};
+#endif /* TwoSum_hpp */
